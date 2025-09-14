6755e8bf6a8b9ad6c2aa1eaa6eae3d1f194e18b7
Mphammed Waleed <m.elsherbeny100@gmail.com>
2025-09-14 12:02:46 +0300

    Finish Validation Package and Tested it

--- test/easy_email_password_validation_test.dart
+++ test/easy_email_password_validation_test.dart
@@ -3,10 +3,50 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:easy_email_password_validation/easy_email_password_validation.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group("Password Validation", () {
+    test('Password Has Number at least 8 numbers', () {
+      expect(EasyEmailPasswordValidation.hasMinimumLength("12345678"), true);
+    });
+
+    test("Password Has Number less than 8 numbers", () {
+      expect(EasyEmailPasswordValidation.hasMinimumLength("1234567"), false);
+    });
+
+    test("Password Has Number more than 8 numbers", () {
+      expect(EasyEmailPasswordValidation.hasMinimumLength("12345"), false);
+    });
+    test("Password Has Lower Letter", (){
+      expect(EasyEmailPasswordValidation.hasLowerLetter("m"), true);
+    });
+    test("Password Hasn't Lower Letter", (){
+      expect(EasyEmailPasswordValidation.hasLowerLetter("N"), false);
+    });
+    test("Password Has Upper Letter", (){
+      expect(EasyEmailPasswordValidation.hasUpperLetter("M"), true);
+    });
+    test("Password Hasn't Upper Letter", (){
+      expect(EasyEmailPasswordValidation.hasUpperLetter("n"), false);
+    });
+    test("Password Has Special Character Letter", (){
+      expect(EasyEmailPasswordValidation.hasSpecialCharacter("@"), true);
+    });
+    test("Password Has Number", (){
+      expect(EasyEmailPasswordValidation.hasANumber("11"), true);
+    });
+    test("Password Hasn't Number", (){
+      expect(EasyEmailPasswordValidation.hasANumber("M"), false);
+    });
+    test("Password Is Empty", () {
+      expect(EasyEmailPasswordValidation.hasANumber(""), false);
+    });
+    test("Password Has Matched Password", (){
+      expect(EasyEmailPasswordValidation.hasMatchedPassword("12345678", "12345678"), true);
+    });
+    test("Password Hasn't Matched Password", (){
+      expect(EasyEmailPasswordValidation.hasMatchedPassword("12345678", "123456781"), false);
+    });
+    test("Password Is Valid", () {
+      expect(EasyEmailPasswordValidation.isPasswordValid("25100@Mn"), true);
+    });
   });
 }
